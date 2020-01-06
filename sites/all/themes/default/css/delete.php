72fccc11c1983114a8925dd2bc1321010a28d703
ph3l1x <rperry@mindimage.net>
2020-01-06 11:24:30 -0700

    hmm

---
+++ sites/all/themes/default/css/delete.php
@@ -0,0 +1,102 @@
+<h2>Find homes in these popular Idaho cities</h2>
+<!-- <iframe src="https://homesinboise.silvercreekrealty.com/guides/" height="300" width="300"></iframe> -->
+<div class="city-guides col-sm-12">
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-photos.global.ssl.fastly.net/imls/640_boomver_1_98745385-1.jpg" width="100%"/></div>
+    <div class="city-name">Star</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?city=35459&sort=importdate&status=A','_blank')">View 113 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/3.jpg" width="100%"/></div>
+    <div class="city-name">Meridian</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Meridian&status=A','_blank')">View 693 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/2.jpg" width="100%"/></div>
+    <div class="city-name">Boise</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Boise&status=A','_blank')">View 960 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/68.jpeg" width="100%"/></div>
+    <div class="city-name">Nampa</div>
+    <div class="city-listings">
+      <button onclick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?city=35459&sort=importdate&status=A','_blank')">View 112 Listings</button>
+    </div>
+  </div>
+  <!-- Next set of 4 -->
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/15.jpg" width="100%"/></div>
+    <div class="city-name">Eagle</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Eagle&status=A','_blank')">View 382 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/81.jpeg" width="100%"/></div>
+    <div class="city-name">Idaho Falls</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Idaho+Falls&status=A','_blank')">View 514 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/49.jpg" width="100%"/></div>
+    <div class="city-name">Twin Falls</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Twin+Falls&status=A','_blank')">View 513 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/2-1.jpg" width="100%"/></div>
+    <div class="city-name">McCall</div>
+    <div class="city-listings">
+      <button onclick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=McCall&status=A','_blank')">View 787 Listings</button>
+    </div>
+  </div>
+  <!-- Next Set of 4 -->
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/81-1.jpeg" width="100%"/></div>
+    <div class="city-name">Kuna</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Kuna&status=A','_blank')">View 171 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/10.jpg" width="100%"/></div>
+    <div class="city-name">Caldwell</div>
+    <div class="city-listings">
+      <button onClick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?citylike=Caldwell&status=A','_blank')">View 429 Listings</button>
+    </div>
+  </div>
+  <div class="city-box col-sm-3">
+    <div class="city-picture"><img src="https://bt-wpstatic.freetls.fastly.net/wp-content/blogs.dir/5777/files/2019/01/82.jpg" width="100%"/></div>
+    <div class="city-name">Boise County</div>
+    <div class="city-listings">
+      <button onclick="window.open('https://homesinboise.silvercreekrealty.com/results-gallery/?countylike=Boise&status=A','_blank')">View 338 Listings</button>
+    </div>
+  </div>
+</div>
+
+<!--------------------------- -->
+
+
+<h2 class="hero_form">Hero Rewards® Savings Starts Here</h2>
+<div class="hero_circles">
+  <div class="col-sm-4"><img src="/sites/all/default/files/icon__dollar.svg">
+    <p>Receive the Most Hero Savings</p>
+  </div>
+
+  <div class="col-sm-4"><img src="/sites/all/default/files/icon__handshake.svg">
+    <p>Receive Extraordinary Service</p>
+  </div>
+
+  <div class="col-sm-4"><img src="/sites/all/default/files/icon__clipboard.svg">
+    <p>And, We'll Also Support Your Fellow Heroes in Need</p>
+  </div>
+</div>
+
---
+++ sites/all/themes/default/templates/fields/field--field-linkedin--homesinboise-team.tpl.php
@@ -0,0 +1,11 @@
+<div class="field field-name-field-facebook field-type-linkedin field-label-hidden">
+    <div class="field-items">
+        <div class="field-item even">
+            <a href="http://<?php print $items[0]['#markup']; ?>" target="_blank">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+<!--                --><?php //var_dump($items); ?>
+            </a>
+        </div>
+    </div>
+</div>
+
